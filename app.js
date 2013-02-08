e58f1b28b06c7935004ecfb9f6c6377fb3ae4b5a
Ed Summers <ehs@pobox.com>
2013-02-08 06:22:29 -0500

    added basic tallying support

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var http = require('http'),
+    moment = require('moment'),
     xml2js = require('xml2js'),
     express = require('express'),
     request = require('request'),
-    socketio = require('socket.io');
+    socketio = require('socket.io'),
+    redis = require('redis').createClient();
+
+/**
+ * Set up the webserver.
+ */
 
 function main() {
   var app = express();
@@ -28,6 +34,12 @@ function main() {
   server.listen(process.env.PORT || 3000);
 }
 
+/**
+ * worldcat will pass newly cataloged items in Worldcat 
+ * to the callback you supply. maxseq is optional, and really
+ * only used when worldcat is called recursively.
+ */
+
 function worldcat(callback, maxseq) {
   url = 'http://experimental.worldcat.org/xwwg/rest/feed?format=json';
   if (maxseq) url += "&start=seq-" + maxseq;
@@ -49,7 +61,13 @@ function worldcat(callback, maxseq) {
  */
 
 function annotate(item, callback) {
+  // add a URL for the item in Worldcat
   item.url = 'http://worldcat.org/oclc/' + item.oclcno;
+
+  // convert epoch time to a Date object
+  item.created = new Date(item.created * 1000); 
+
+  // get some information about the organization
   url = 'http://www.worldcat.org/webservices/registry/lookup/Institutions/oclcSymbol/' + item.instsym + '?serviceLabel=content';
   request.get({url: url}, function (e, r, xml) {
     xml2js.parseString(xml, function (e, r) {
@@ -59,9 +77,24 @@ function annotate(item, callback) {
       } catch(err) {
         console.log("no org record for " + item.instsym);
       }
+      tally(item);
       callback(item);
     });
   });
+}
+
+/**
+ * Keep track of some stats using Redis
+ */
+
+function tally(item) {
+  var org = "org:" + item.instsym;
+  var m = moment(item.created);
+
+  redis.zincrby("daily_" + m.format('YYYYMMDD'), 1, org, function() {});
+
+  redis.hset(org, "name", item.instname, function() {});
+  redis.hset(org, "url", item.url, function() {});
 }
 
 if (! module.parent) {
